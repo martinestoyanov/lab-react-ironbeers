c607533038213bb6e46625edb4dd866705895a02
Martin Stoyanov <martinestoyanov@gmail.com>
2021-05-21 20:46:24 -0400

    Basic Routes Established

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from `react-router-dom`;
+import RandomBeer from './components/RandomBeer/RandomBeer';
+import NewBeer from './components/NewBeer/NewBeer';
+import Home from './components/Home/Home';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/beers" component={}/>
+        <Route exact path="/random" component={RandomBeer}/>
+        <Route exact path="/new-beer"  component={NewBeer}/>
+        <Route exact path="/" component={Home}/>
+      </Switch>
     </div>
   );
 }
