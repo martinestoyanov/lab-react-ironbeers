e2f5bb39c93033fb984dd54f2d14b9880a41c7ff
Martin Stoyanov <martinestoyanov@gmail.com>
2021-05-22 14:09:39 -0700

    beers with links to each beer working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import RandomBeer from './components/RandomBeer/RandomBeer';
 import NewBeer from './components/NewBeer/NewBeer';
 import Home from './components/Home/Home';
 import Beers from './components/Beers/Beers';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/beers" component={Beers} />
-        <Route exact path="/random" component={RandomBeer} />
-        <Route exact path="/new-beer" component={NewBeer} />
-        <Route exact path="/" component={Home} />
-      </Switch>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    beers: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get(`https://ih-beers-api2.herokuapp.com/beers`)
+      .then((responseFromApi) => {
+        this.setState({ beers: responseFromApi.data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/beers"
+            render={() => <Beers beers={this.state.beers} />}
+          />
+          <Route exact path="/random" component={RandomBeer} />
+          <Route exact path="/new-beer" component={NewBeer} />
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </div>
+    );
+  }
 }
-
-export default App;
--- src/components/Beers/Beers.js
+++ src/components/Beers/Beers.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react'
-import NavBar from '../NavBar/NavBar'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import NavBar from '../NavBar/NavBar';
 
 export default class Beers extends Component {
-    render() {
-        return (
-            <div>
-                <NavBar />
-                
-            </div>
-        )
-    }
+  render() {
+    const beersToRender = this.props.beers.map((beerData) => (
+      <div>
+        <Link to="/beers/${beerData._id}">
+          <img src={beerData.image_url} />
+          <p>{beerData.name}</p>
+        </Link>
+      </div>
+    ));
+    return (
+      <div>
+        <NavBar />
+        {beersToRender}
+      </div>
+    );
+  }
 }
